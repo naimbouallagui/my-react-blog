8abdbc76085daa254532dc58a259287e38d43932
Naim <naim.bouallagui.8@gmail.com>
2020-01-17 15:04:54 +0100

    fix add and delete comment article in reducer

--- src/Reducers/ArticleReducer.js
+++ src/Reducers/ArticleReducer.js
@@ -26,7 +26,7 @@ export const ArticleReducer = (state, action) => {
             return { ...state, articles: allArticles };
         case 'UPDATE_ARTICLE':
             const articles = JSON.parse(localStorage.getItem('listArticles'));
-            const index = articles.map(a => { return a.id}).indexOf(action.article.id);
+            const index = articles.map(a => { return a.id }).indexOf(action.article.id);
             articles[index] = action.article;
             localStorage.setItem('listArticles', JSON.stringify(articles));
             return { ...state, articles };
@@ -34,6 +34,27 @@ export const ArticleReducer = (state, action) => {
         case 'ALL_ARTICLES':
             state.articles = JSON.parse(localStorage.getItem('listArticles'));
             return state
+        case 'ADD_COMMENT':
+            const newCommentState = {
+                ...state, comments: [
+                    ...state.comments, {
+                        title: action.comment.title,
+                        comment: action.comment.comment,
+                        owner: action.comment.owner,
+                        id: uuid()
+                    }
+                ]
+            }
+
+            localStorage.setItem('listComments', JSON.stringify(newCommentState.comments))
+            return newCommentState;
+            case 'REMOVE_COMMENT':
+            // const index = state.comments.map((e) => { return e.id; }).indexOf(action.id);
+            // const allComments = [...state.comments]
+            // allComments.splice(index, 1);
+            const allComments = state.comments.filter(e => e.id !== action.id)
+            localStorage.setItem('listComments', JSON.stringify(allComments));
+            return { ...state, comments: allComments };
         default:
             return state
     }
